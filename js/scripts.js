e14bb8980060ae6aad3456e77befb3e6726fe577
MoiseevGennadii <144433348+MoiseevGennadii@users.noreply.github.com>
2023-11-09 21:28:09 +0300

    1

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+function openInstagram() {
+    window.location.href = "instagram://user?username=domik_knyazya";
+};
+
+function openViber() {
+    window.location.href = "viber://chat?number=375298990408"
+};
+
+function openWhatsapp() {
+    window.location.href = "https://api.whatsapp.com/send?phone=375298990408"
+};
+
+function arrow() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
+
+// window.addEventListener('click', function (event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// });
